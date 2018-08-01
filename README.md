0626789dd2d443ae58049df28b153509f1fd5f2e
DESKTOP-KKF2OGA\kaijer <grahary@gmail.com>
2018-08-01 22:16:42 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WS_Notebook_Windows
+
+Java, Swing, JDBC를 활용한 메모 관리 프로그램
+
+
+
+- - -
---
+++ src/kr/pe/kaijer/Main.java
@@ -0,0 +1,7 @@
+package kr.pe.kaijer;
+
+import javax.swing.*;
+
+public class Main {
+    private JPanel jPanel;
+}
